3daeae1fa62939e138fc9de1cc86f60611578521
UOA1992 <UOA1992@github.com>
2018-09-16 15:40:38 -0400

    6th Commit

---
+++ WeatherApp/Struct/ForecastWeatherStruct.swift
@@ -0,0 +1,9 @@
+//
+//  ForecastWeatherStruct.swift
+//  WeatherApp
+//
+//  Created by Edward L Richardson on 9/16/18.
+//  Copyright © 2018 Edward L Richardson. All rights reserved.
+//
+
+import Foundation
