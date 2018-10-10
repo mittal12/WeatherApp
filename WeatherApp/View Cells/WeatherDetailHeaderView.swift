21b717aaf31b6737c1974f7932a41fd01997bddf
UOA1992 <UOA1992@github.com>
2018-10-09 20:36:42 -0400

    10 9 18: updated WeatherDetailsHeaderView with function statement and updated the constraints for the WeatherDetailsHeaderView.xib file.

--- WeatherApp/View Cells/WeatherDetailHeaderView.swift
+++ WeatherApp/View Cells/WeatherDetailHeaderView.swift
@@ -1,5 +1,5 @@
 //
-//  WhetherDetailHeaderView.swift
+//  WeatherDetailHeaderView.swift
 //  WeatherApp
 //
 //  Created by Ashish Mittal on 09/10/18.
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class WhetherDetailHeaderView: UITableViewCell {
+class WeatherDetailHeaderView: UITableViewCell {
 
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var temperature: UILabel!
+    @IBOutlet weak var summary: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func setModel(weatherIcon:UIImage,cityName:String,temperature:Int,summary:String) {
+        return 
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
