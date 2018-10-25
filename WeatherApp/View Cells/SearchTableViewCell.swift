3a1c4b75ffea06eabe55f0e455ec95db1c92b49f
Ashish Mittal <ashishmittal@Ashishs-MacBook-Pro.local>
2018-10-25 07:30:00 +0530

    search table View

---
+++ WeatherApp/View Cells/SearchTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchTableViewCell.swift
+//  WeatherApp
+//
+//  Created by Ashish Mittal on 25/10/18.
+//  Copyright © 2018 Edward L Richardson. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cityName: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        // Configure the view for the selected state
+    }
+    
+}
--- WeatherApp/ViewControllers/SearchVC.swift
+++ WeatherApp/ViewControllers/SearchVC.swift
@@ -12,21 +12,52 @@ import CoreLocation
 class SearchVC: UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
-    @IBOutlet weak var GrayActivityIndicator: UIActivityIndicatorView!
+  
+    @IBOutlet weak var tableView: UITableView!
     
-    
+    let modelArray:[String] = ["New York","California","Boston","Washington","dallas","utah","texas","San Fransisco","Las Vegas","Denver"]
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        let nib = UINib(nibName: "SearchTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "SearchTableViewCell")
+        // same as delegation pattern
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension SearchVC:UITableViewDelegate, UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return modelArray.count
+    }
     
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchTableViewCell") as! SearchTableViewCell
+        
+        // take out model from the array
+        let model = modelArray[indexPath.row]
+        cell.cityName.text = model
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44
+    }
+    
+}
 
+
+extension SearchVC:UISearchBarDelegate{
+    
 }
